4f453032b3ab6b564ab7ecb82e9fe22f2495550f
Natalia Kimel <nata@rus9n.com>
2024-12-14 00:54:38 +0300

    setup github-pages

---
+++ build/static/js/3.5fc10237.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[3],{540:function(e,s,n){"use strict";n.r(s);var t=n(2);n(0);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=3.5fc10237.chunk.js.map
